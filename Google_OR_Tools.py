d1cad55be9dc0be0271efaa21951a88ddb550800
An Vo <vokhanhan25@gmail.com>
2021-04-27 19:55:00 +0700

    update output

--- Google_OR_Tools.py
+++ Google_OR_Tools.py
@@ -1,7 +1,7 @@
 from ortools.algorithms import pywrapknapsack_solver
 import data
 import time
-
+import os
 def main():
     
     inp = data.input_data()
@@ -10,16 +10,16 @@ def main():
         pywrapknapsack_solver.KnapsackSolver.
         KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER, 'KnapsackExample')
 
-    for file_name in range(len(inp)):
+    for file_name in range(27, len(inp)):
         
-        with open(inp[file_name] + ".kp") as inp_file:
+        with open("data/" + inp[file_name] + ".kp") as inp_file:
             read_file = inp_file.read().split('\n')
             
         number_items = (int)(read_file[1])
         capacities = [(int)(read_file[2])]
         values = []
         weights = [[]]
-
+        print(file_name, number_items)
         for i in range(4, number_items + 4):
             x = read_file[i].split(" ")[0]
             y = read_file[i].split(" ")[1]
@@ -37,20 +37,22 @@ def main():
         # packed_items = []
         # packed_weights = []
         total_weight = 0
-
+        
         for i in range(len(values)):
                 if solver.BestSolutionContains(i):
                     # packed_items.append(i)
                     # packed_weights.append(weights[0][i])
                     total_weight += weights[0][i]
-        with open("output/OR-Tools/test" + str(file_name) +".txt", "w") as output_file:
+        filename = "/foo/bar/baz.txt"
+        
+        with open("output/OR-Tools/" + inp[file_name] + ".txt", "w") as output_file:
             output_file.write('File name: ' + inp[file_name] + "\n")
             output_file.write ('Number of items: ' + str(number_items) + "\n")
             output_file.write('Total value: ' + str(computed_value) + "\n")
             
             output_file.write('Total weight: ' +  str(total_weight) + "\n")
             output_file.write('Capacity: ' + str(capacities[0]) + "\n")
-            output_file.write('Runtime: ' + str(time_step * 1000) + " miliseconds\n")
+            output_file.write('Runtime: ' + str(time_step) + " seconds\n")
             # print('Packed items:', packed_items)
             # print('Packed_weights:', packed_weights)
             # print('/--------------------------/\n\n')
